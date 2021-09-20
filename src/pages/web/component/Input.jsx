f367e51cc62951b802f993b19c2f8df0f3c05ca5
WIN-5H00BF86B59\Administrator <790478146@qq.com>
2021-09-20 14:53:11 +0800

    解决绑定value的疑惑

--- src/pages/web/component/Input.jsx
+++ src/pages/web/component/Input.jsx
@@ -10,17 +10,20 @@ const Input = props => {
 //   </div>
 // );
 
+// 在学习的过程中我在想为啥不绑定onChange事件呢，后来发现不是没有绑定，...otherProps不就是 绑定了value和onchange事件嘛
+
 class CustomizeInput extends React.Component {
   constructor(props) {
     super(props);
     this.state = {};
   }
   render() {
-    const {value = "", ...otherProps} = this.props;
+    const { ...otherProps} = this.props;
+    console.log('---为什么绑定valuevalue-', {...otherProps})
     return (
       <div style={{padding: 10}}>
         <Input style={{outline: "none"}}
-         value={value}
+        //  value={value}
          {...otherProps} />
       </div>
     );
